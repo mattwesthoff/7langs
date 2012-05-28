97d70c91ba07f46fcf6164fe9140c753733ba83f
Matt Westhoff <mattwesthoff@gmail.com>
2012-05-27 21:34:34 -0500

    check if the file exists

--- ruby/day2/grep.rb
+++ ruby/day2/grep.rb
@@ -1,4 +1,6 @@
+#these could be inputs
 regex = /a/
 file = "test.txt"
 
+raise IOError, "Couldn't find file: #{file}" unless File.file?(file)
 File.readlines(file).each_with_index { |l,i| puts "#{i+1}: #{l}" if l =~ regex }
