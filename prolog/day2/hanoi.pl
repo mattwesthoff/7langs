5db05f737519d0e40cf1f1265d1e53917ae6b5e4
Matt Westhoff <mattwesthoff@gmail.com>
2012-12-06 17:26:34 -0600

    hanoi solution
    can't believe I've never thought of this as such a good example of
    recursion, apparently I forgot about it from college, or my school
    didn't teach this one.

--- prolog/day2/hanoi.pl
+++ prolog/day2/hanoi.pl
@@ -1,1 +1,7 @@
-hanoi(State, Solution) :- 
+hanoi(1, FromPeg, ToPeg, _) :- write(FromPeg), write(' -> '), write(ToPeg), nl.
+
+hanoi(Discs, FromPeg, ToPeg, OtherPeg) :- 
+	Discs1 is Discs - 1,
+	hanoi(Discs1, FromPeg, OtherPeg, ToPeg),
+	hanoi(1, FromPeg, ToPeg, _),
+	hanoi(Discs1, OtherPeg, ToPeg, FromPeg).
