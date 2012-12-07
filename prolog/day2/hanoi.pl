eb2d33e79d5d16228fcc8f561fbf753242813f24
Matt Westhoff <mattwesthoff@gmail.com>
2012-12-06 18:26:08 -0600

    working on visualization of the moves

--- prolog/day2/hanoi.pl
+++ prolog/day2/hanoi.pl
@@ -1,7 +1,20 @@
-hanoi(1, FromPeg, ToPeg, _) :- write(FromPeg), write(' -> '), write(ToPeg), nl.
+printstate([Col1|[Col2,Col3]]) :- printstate(Col1, Col2, Col3).
 
-hanoi(Discs, FromPeg, ToPeg, OtherPeg) :- 
+printstate([],[],[]) :- nl.
+
+printstate([Row1|Tail1], [Row2|Tail2], [Row3|Tail3]) :- 
+	write(Row1), write('|'), write(Row2), write('|'), write(Row3), nl,
+	printstate(Tail1, Tail2, Tail3).
+
+hanoi(N) :- printstate([['_    ', '__   ', '___  ', '____ '],['     ','     ','     ','     '],['     ','     ','     ','     ']]).
+
+move([Col1|[Col2|Col3]], From, To) :-
+	write('finish').
+
+solve(1, FromPeg, ToPeg, _) :- write(FromPeg), write(' -> '), write(ToPeg), nl.
+
+solve(Discs, FromPeg, ToPeg, OtherPeg) :- 
 	Discs1 is Discs - 1,
-	hanoi(Discs1, FromPeg, OtherPeg, ToPeg),
-	hanoi(1, FromPeg, ToPeg, _),
-	hanoi(Discs1, OtherPeg, ToPeg, FromPeg).
+	solve(Discs1, FromPeg, OtherPeg, ToPeg),
+	solve(1, FromPeg, ToPeg, _),
+	solve(Discs1, OtherPeg, ToPeg, FromPeg).
