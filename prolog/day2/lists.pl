1de78e9fa5148ab66e4bfceec7fb59f3afb0b86f
Matt Westhoff <mattwesthoff@gmail.com>
2012-12-11 00:54:37 -0600

    woah... also, somethings still not right with sort

--- prolog/day2/lists.pl
+++ prolog/day2/lists.pl
@@ -7,3 +7,7 @@ reverse([X|Rest], Result, Output) :-
 smallest([X], X). %last item in a list is the smallest
 smallest([Head|Rest], Output) :- smallest(Rest, Val), Val =< Head, Output is Val.
 smallest([Head|Rest], Output) :- smallest(Rest, Val), Head < Val, Output is Head.
+
+mysort([X], [X]).
+mysort([Head|Rest], Output) :- sort(Rest, [Next|NextRest]), Next > Head, append([Head], [Next], Output).
+mysort([Head|Rest], Output) :- sort(Rest, [Next|NextRest]), Next =< Head, append([Next], [Head], Output).
