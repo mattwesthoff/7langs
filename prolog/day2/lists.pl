92377ee3ac9fba173f4dbb2ff0bb78c5dd543e4b
Matt Westhoff <mattwesthoff@gmail.com>
2012-12-11 00:56:41 -0600

    ok fixed the missing the rest of the list issue, but it's still not quite right

--- prolog/day2/lists.pl
+++ prolog/day2/lists.pl
@@ -1,13 +1,13 @@
 reverse([], Output, Output).
-reverse(Input, Output) :- reverse(Input, [], Output).
 reverse([X|Rest], Result, Output) :-
 	append([X], Result, NewOut),
 	reverse(Rest, NewOut, Output).
+reverse(Input, Output) :- reverse(Input, [], Output).
 
 smallest([X], X). %last item in a list is the smallest
 smallest([Head|Rest], Output) :- smallest(Rest, Val), Val =< Head, Output is Val.
 smallest([Head|Rest], Output) :- smallest(Rest, Val), Head < Val, Output is Head.
 
 mysort([X], [X]).
-mysort([Head|Rest], Output) :- sort(Rest, [Next|NextRest]), Next > Head, append([Head], [Next], Output).
-mysort([Head|Rest], Output) :- sort(Rest, [Next|NextRest]), Next =< Head, append([Next], [Head], Output).
+mysort([Head|Rest], Output) :- sort(Rest, [Next|NextRest]), Next > Head, append([Head], [Next|NextRest], Output).
+mysort([Head|Rest], Output) :- sort(Rest, [Next|NextRest]), Next =< Head, append([Next], [Head|NextRest], Output).
