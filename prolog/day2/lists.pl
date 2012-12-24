4b4389bb27531a101471c1b7ca250153c4fbed3b
Matt Westhoff <mattwesthoff@gmail.com>
2012-12-23 22:05:50 -0500

    sorting sort of works, pretty inefficient though, and by that I mean... super inefficient.

--- prolog/day2/lists.pl
+++ prolog/day2/lists.pl
@@ -8,7 +8,19 @@ smallest([X], X). %last item in a list is the smallest
 smallest([Head|Rest], Output) :- smallest(Rest, Val), Val =< Head, Output is Val.
 smallest([Head|Rest], Output) :- smallest(Rest, Val), Head < Val, Output is Head.
 
-mysort([X], [X]).
-%urg, not this easy, I think I need to sort again?? gotta work the traces.
-mysort([Head|Rest], Output) :- mysort(Rest, [Next|NextRest]), Next > Head, append([Head], [Next|NextRest], Output).
-mysort([Head|Rest], Output) :- mysort(Rest, [Next|NextRest]), Next =< Head, append([Next], [Head|NextRest], Output).
+remove(_, [], Result, Result).
+remove(Elem, [H|Arr], Result, Output) :- H = Elem, remove(Elem, Arr, Result, Output).
+remove(Elem, [H|Arr], Result, Output) :- 
+	H \= Elem, 
+	append(Result, [H], NResult),
+	remove(Elem, Arr, NResult, Output).
+remove(Elem, Arr, Output) :- remove(Elem, Arr, [], Output).
+
+
+mysort([X|[]], Curr, Output) :- append(Curr, [X], Output).
+mysort(Arr, Curr, Output) :- 
+	smallest(Arr, Smallest), 
+	append(Curr, [Smallest], NewCurr),
+	remove(Smallest, Arr, Rest),
+	mysort(Rest, NewCurr, Output).
+mysort(Arr, Output) :- mysort2(Arr, [], Output).
