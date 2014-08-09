84108fe0b334ed8a74a42e726d96ce2a79bcdf1f
Matt Westhoff <mattwesthoff@gmail.com>
2014-08-09 14:45:24 -0700

    everything but draws

--- scala/day1/tictactoe.scala
+++ scala/day1/tictactoe.scala
@@ -10,13 +10,26 @@ object TicTacToe {
 	}
 
 	def isGameOver(game : Array[Array[String]]) : Boolean = {
-		game.foreach { row => {
-			val winner = TicTacToe.isRowWinner(row)
-			if (winner == true) return (true)
-		}}
-		if (isRowWinner(Array(game(0)(0), game(1)(0), game(2)(0)))) return (true)
-		if (isRowWinner(Array(game(0)(1), game(1)(1), game(2)(1)))) return (true)
-		if (isRowWinner(Array(game(0)(2), game(1)(2), game(2)(2)))) return (true)
+		game.foreach { row => if (TicTacToe.isRowWinner(row)) return (true) }
+		if (isRowWinner(for (row <- game) yield row(0) )) return (true)
+		if (isRowWinner(for (row <- game) yield row(1) )) return (true)
+		if (isRowWinner(for (row <- game) yield row(2) )) return (true)
+
+		var i = 0
+		val diaglr = for (row <- game) yield { 
+			i += 1
+			row(i - 1)
+		}
+
+		if (isRowWinner(diaglr)) return (true)
+
+		i = 2
+		val diagrl = for (row <- game) yield { 
+			i -= 1
+			row(i + 1)
+		}
+		if (isRowWinner(diagrl)) return true
+
 		return (false)
 	}
 }
@@ -39,6 +52,13 @@ class TicTacToeSpec extends FlatSpec with Matchers {
 
   	it should "be over if there are three x's in a column" in {
   		var game = Array(Array("o","x","o"),Array("","x","o"),Array("o","x","o"))
+    	TicTacToe.isGameOver(game) should be (true)
+  	}
+
+  	it should "be over if there are three-of-a-kind in a diagonal" in {
+  		var game = Array(Array("o","x","o"),Array("","o","x"),Array("","x","o"))
+    	TicTacToe.isGameOver(game) should be (true)
+    	game = Array(Array("o","x","x"),Array("","x",""),Array("x","",""))
     	TicTacToe.isGameOver(game) should be (true)
   	}
 }
