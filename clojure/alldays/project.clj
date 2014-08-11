902860bdc9f2c75e74c8d33757e8de924df6263b
Matt Westhoff <mattwesthoff@gmail.com>
2014-08-10 19:15:38 -0700

    updating project.clj

--- clojure/alldays/project.clj
+++ clojure/alldays/project.clj
@@ -1,6 +1,6 @@
 (defproject day1 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "7 langs in infinity time - clojure section"
+  :url "http://mattsfakescience.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
