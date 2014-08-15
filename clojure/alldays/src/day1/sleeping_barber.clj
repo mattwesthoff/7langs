ea9503b318fb780ca7547e8ad7362c77c2cdca1e
Matt Westhoff <mattwesthoff@gmail.com>
2014-08-14 20:50:14 -0700

    sleeping barber but no walkouts??

--- clojure/alldays/src/day1/sleeping_barber.clj
+++ clojure/alldays/src/day1/sleeping_barber.clj
@@ -9,10 +9,43 @@
 	; if the chairs are fill all new customers turn away
 	; haircuts take 20 ms
 	; after a customer receives a haircut he gets up a leaves
-	
+
+(def running (ref true))
 (def available-chairs (ref 3))
+(def satisfied-customers (ref 0))
+(def barber-working? (ref false))
 (def walkouts (ref 0))
 
+; customer behavior
 (defn customer-enters []
-	(if (> @available-chairs 0)
-		(dosync (alter available-chairs dec)) (dosync (alter walkouts inc))))
+	(dosync (if (> @available-chairs 0)
+		(alter available-chairs dec) 
+		(alter walkouts inc))))
+
+; customer generator
+(defn marketing []
+	(future (while @running 
+		(Thread/sleep (+ 10 (rand-int 20)))
+		customer-enters)))
+
+; barber behavior
+(defn cut-hair [sleep]
+	(dosync 
+		(ref-set barber-working? true)
+		(alter available-chairs inc))
+	(Thread/sleep sleep)
+	(dosync 
+		(alter satisfied-customers inc)
+		(ref-set barber-working? false)))
+
+; barber loop
+(defn barber []
+	(future (while (or @running (< @available-chairs 3)) (if (not @barber-working?) (cut-hair 20)))))
+
+(defn -main []
+	(marketing)
+	(barber)
+	(Thread/sleep 60000)
+	(dosync (ref-set running false))
+	(print "haircuts: " @satisfied-customers " walkouts: " @walkouts "\n")
+	(shutdown-agents))
--- clojure/alldays/test/day1/sleeping_barber_test.clj
+++ clojure/alldays/test/day1/sleeping_barber_test.clj
@@ -4,14 +4,22 @@
 
 (deftest new-customer-fills-seat-if-available
 	(testing "a customer walking in the door occupies a seat if one is available"
-		(is (= 1 (dosync (ref-set available-chairs 1))))
+		(dosync (ref-set available-chairs 1))
 		(customer-enters)
 		(is (= 0 @available-chairs))))
 
 (deftest new-customer-walks-out-if-no-seats
 	(testing "no seats means no customer"
-		(is (= 0 (dosync (ref-set available-chairs 0))))
-		(is (= 0 (dosync (ref-set walkouts 0))))
+		(dosync (ref-set available-chairs 0))
+		(dosync (ref-set walkouts 0))
 		(customer-enters)
 		(is (= 1 @walkouts))
 		(is (= 0 @available-chairs))))
+
+(deftest hair-cut-incs-available-chairs-and-satisfied-customers
+	(testing "cutting hair frees up a waiting spot and satisfies"
+		(dosync (ref-set available-chairs 0))
+		(dosync (ref-set satisfied-customers 0))
+		(cut-hair 0)
+		(is (= 1 @available-chairs))
+		(is (= 1 @satisfied-customers))))
